7daead75b830d8f171f03f9f917781bea70fac26
Berserrk <152796199+Berserrk@users.noreply.github.com>
2025-02-11 16:15:47 +0100

    Update todrop.py

--- todrop.py
+++ todrop.py
@@ -1,7 +1,23 @@
 import streamlit as st
 from streamlit_cytoscapejs import cytoscape
 
-# Example showing different layouts
+st.set_page_config(layout="wide")
+
+st.markdown("""
+    <style>
+        .block-container { padding: 1rem; }
+        .stCytoscape { height: 800px !important; width: 100% !important; }
+    </style>
+""", unsafe_allow_html=True)
+
+# Sample elements
+elements = [
+    {'data': {'id': 'a', 'label': 'Node A'}},
+    {'data': {'id': 'b', 'label': 'Node B'}},
+    {'data': {'source': 'a', 'target': 'b'}}
+]
+
+# Available layouts
 layouts = {
     'random': {'name': 'random'},
     'grid': {'name': 'grid'},
@@ -16,11 +32,34 @@ layouts = {
     'klay': {'name': 'klay'}
 }
 
-# Layout selector
 selected_layout = st.selectbox('Select Layout', list(layouts.keys()))
+
+stylesheet = [
+    {
+        'selector': 'node',
+        'style': {
+            'background-color': '#666',
+            'label': 'data(label)',
+            'width': 50,
+            'height': 50,
+            'font-size': '12px',
+            'text-valign': 'center',
+            'text-halign': 'center'
+        }
+    },
+    {
+        'selector': 'edge',
+        'style': {
+            'width': 2,
+            'curve-style': 'bezier',
+            'line-color': '#999'
+        }
+    }
+]
 
 cytoscape(
     elements=elements,
+    stylesheet=stylesheet,
     layout=layouts[selected_layout],
     key='graph'
 )
