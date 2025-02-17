1d70d5c025777e1656b2f0ab21f404776d1a0f07
Berserrk <152796199+Berserrk@users.noreply.github.com>
2025-02-17 08:36:10 +0100

    Update todrop.py

--- todrop.py
+++ todrop.py
@@ -1,65 +1,267 @@
-import streamlit as st
-from streamlit_cytoscapejs import cytoscape
-
-st.set_page_config(layout="wide")
-
-st.markdown("""
-    <style>
-        .block-container { padding: 1rem; }
-        .stCytoscape { height: 800px !important; width: 100% !important; }
-    </style>
-""", unsafe_allow_html=True)
-
-# Sample elements
-elements = [
-    {'data': {'id': 'a', 'label': 'Node A'}},
-    {'data': {'id': 'b', 'label': 'Node B'}},
-    {'data': {'source': 'a', 'target': 'b'}}
-]
-
-# Available layouts
-layouts = {
-    'random': {'name': 'random'},
-    'grid': {'name': 'grid'},
-    'circle': {'name': 'circle'},
-    'concentric': {'name': 'concentric'},
-    'breadthfirst': {'name': 'breadthfirst'},
-    'cose': {'name': 'cose'},
-    'cola': {'name': 'cola'},
-    'euler': {'name': 'euler'},
-    'spread': {'name': 'spread'},
-    'dagre': {'name': 'dagre'},
-    'klay': {'name': 'klay'}
-}
-
-selected_layout = st.selectbox('Select Layout', list(layouts.keys()))
-
-stylesheet = [
+{
+  "editor.fontFamily": "JetBrains Mono",
+  "editor.fontSize": 14,
+  "editor.fontLigatures": true,
+  "notebook.output.fontFamily": "JetBrains Mono",
+  "notebook.output.fontSize": 14,
+  "editor.formatOnSave": true,
+  "editor.suggest.insertMode": "replace",
+  "terminal.integrated.fontFamily": "JetBrains Mono",
+  "editor.linkedEditing": true,
+  "javascript.updateImportsOnFileMove.enabled": "always",
+  "window.zoomLevel": -1.623400328908832,
+  "terminal.integrated.gpuAcceleration": "off",
+  "window.commandCenter": false,
+  "launch": {},
+  "[json]": {},
+  "editor.minimap.enabled": false,
+  "workbench.iconTheme": "material-icon-theme",
+  "update.showReleaseNotes": false,
+  "zenMode.hideLineNumbers": false,
+  "editor.lineNumbers": "relative",
+  "vim.leader": "<Space>",
+  "vim.hlsearch": true,
+  "vim.normalModeKeyBindingsNonRecursive": [
+    // NAVIGATION
+    // switch b/w buffers
     {
-        'selector': 'node',
-        'style': {
-            'background-color': '#666',
-            'label': 'data(label)',
-            'width': 50,
-            'height': 50,
-            'font-size': '12px',
-            'text-valign': 'center',
-            'text-halign': 'center'
-        }
+      "before": [
+        "<S-h>"
+      ],
+      "commands": [
+        ":bprevious"
+      ]
     },
     {
-        'selector': 'edge',
-        'style': {
-            'width': 2,
-            'curve-style': 'bezier',
-            'line-color': '#999'
-        }
+      "before": [
+        "<S-l>"
+      ],
+      "commands": [
+        ":bnext"
+      ]
+    },
+    // splits
+    {
+      "before": [
+        "leader",
+        "v"
+      ],
+      "commands": [
+        ":vsplit"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "s"
+      ],
+      "commands": [
+        ":split"
+      ]
+    },
+    // panes
+    {
+      "before": [
+        "leader",
+        "h"
+      ],
+      "commands": [
+        "workbench.action.focusLeftGroup"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "j"
+      ],
+      "commands": [
+        "workbench.action.focusBelowGroup"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "k"
+      ],
+      "commands": [
+        "workbench.action.focusAboveGroup"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "l"
+      ],
+      "commands": [
+        "workbench.action.focusRightGroup"
+      ]
+    },
+    // NICE TO HAVE
+    {
+      "before": [
+        "leader",
+        "w"
+      ],
+      "commands": [
+        ":w!"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "q"
+      ],
+      "commands": [
+        ":q!"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "x"
+      ],
+      "commands": [
+        ":x!"
+      ]
+    },
+    {
+      "before": [
+        "[",
+        "d"
+      ],
+      "commands": [
+        "editor.action.marker.prev"
+      ]
+    },
+    {
+      "before": [
+        "]",
+        "d"
+      ],
+      "commands": [
+        "editor.action.marker.next"
+      ]
+    },
+    {
+      "before": [
+        "<leader>",
+        "c",
+        "a"
+      ],
+      "commands": [
+        "editor.action.quickFix"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "f"
+      ],
+      "commands": [
+        "workbench.action.quickOpen"
+      ]
+    },
+    {
+      "before": [
+        "leader",
+        "p"
+      ],
+      "commands": [
+        "editor.action.formatDocument"
+      ]
+    },
+    {
+      "before": [
+        "g",
+        "h"
+      ],
+      "commands": [
+        "editor.action.showDefinitionPreviewHover"
+      ]
     }
-]
-
-cytoscape(
-    elements=elements,
-    stylesheet=stylesheet,
-    layout=layouts[selected_layout],
-    key='graph'
-)
+  ],
+  "vim.visualModeKeyBindings": [
+    // Stay in visual mode while indenting
+    {
+      "before": [
+        "<"
+      ],
+      "commands": [
+        "editor.action.outdentLines"
+      ]
+    },
+    {
+      "before": [
+        ">"
+      ],
+      "commands": [
+        "editor.action.indentLines"
+      ]
+    },
+    // Move selected lines while staying in visual mode
+    {
+      "before": [
+        "J"
+      ],
+      "commands": [
+        "editor.action.moveLinesDownAction"
+      ]
+    },
+    {
+      "before": [
+        "K"
+      ],
+      "commands": [
+        "editor.action.moveLinesUpAction"
+      ]
+    },
+    // toggle comment selection
+    {
+      "before": [
+        "leader",
+        "c"
+      ],
+      "commands": [
+        "editor.action.commentLine"
+      ]
+    }
+  ],
+  "[typescriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "go.toolsManagement.autoUpdate": true,
+  "[typescript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[jsonc]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "editor.cursorWidth": 4,
+  "workbench.settings.applyToAllProfiles": [
+    "workbench.colorCustomizations"
+  ],
+  "workbench.colorCustomizations": {
+    "editorCursor.foreground": "#524e4c",
+    "terminal.background": "#00000000"
+  },
+  "workbench.colorTheme": "Solarized Autumn",
+  "terminal.integrated.defaultProfile.windows": "",
+  "apc.header": {
+    "height": 36
+  },
+  "apc.listRow": {
+    "height": 24
+  },
+  "apc.styleSheet": { "stylesheet": Unknown word.
+    ".title-label > h2": "display": "none",
+    ".editor-actions": "display": "none",
+    ".pane-body": "padding": "0 8px",
+    ".pane-header": "padding": "0 8px",
+  },
+  "apc.electron": {
+    "opacity": 0.45,
+    "transparent": true,
+    "backgroundColor": "#00000000",
+    "vibrancy": "ultra-dark"
+  }
+}
