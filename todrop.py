13ed0727c9388decc671f8ce7f20d3021222b4ce
Berserrk <152796199+Berserrk@users.noreply.github.com>
2025-03-05 15:49:22 +0100

    Update todrop.py

--- todrop.py
+++ todrop.py
@@ -1,4 +1,1 @@
-Dear Sacha,
-Please find attached the table containing detailed information about the various thresholds specified in the scenario. I have compiled the relevant information and included additional details regarding the AUM thresholds below the table.
-If you have any questions or need further clarification, please do not hesitate to contact me.
-Best regards,
+
