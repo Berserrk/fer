b9bc8c72835447e2e7fc480948f37330153e552c
Berserrk <152796199+Berserrk@users.noreply.github.com>
2025-04-15 15:21:13 +0200

    Update todrop.py

--- todrop.py
+++ todrop.py
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Create Streamlit config directory and config file
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-port = \$PORT\n\
-enableXsrfProtection = false\n\
-enableCORS = false\n\
-" > ~/.streamlit/config.toml
+cat <<EOF > ~/.streamlit/config.toml
+[server]
+headless = true
+port = $PORT
+enableXsrfProtection = false
+enableCORS = false
+EOF
 
-# Install Python dependencies using Poetry (optional if already done)
 poetry install
-
-# Run the Streamlit app using Poetry environment
 poetry run streamlit run app.py
